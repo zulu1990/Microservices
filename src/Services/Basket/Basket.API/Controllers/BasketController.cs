be0577296d5b6a0d89c865ee71eec3e89db3f8a7
Archili Zivzivadze <Archili.Zivzivadze@nike.com>
2024-10-11 18:14:32 +0400

    comment code

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -10,16 +10,16 @@ namespace Basket.API.Controllers
     public class BasketController : ControllerBase
     {
         private readonly IBasketRepository _repository;
-        private readonly DiscountGrpcService _discountGrpcService;
-        private readonly IPublishEndpoint _publishEndpoint;
-        private readonly IMapper _mapper;
+        //private readonly DiscountGrpcService _discountGrpcService;
+        //private readonly IPublishEndpoint _publishEndpoint;
+        //private readonly IMapper _mapper;
 
-        public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService, IPublishEndpoint publishEndpoint, IMapper mapper)
+        public BasketController(IBasketRepository repository/*, DiscountGrpcService discountGrpcService, IPublishEndpoint publishEndpoint, IMapper mapper*/)
         {
             _repository = repository;
-            _discountGrpcService = discountGrpcService;
-            _publishEndpoint = publishEndpoint;
-            _mapper = mapper;
+            //_discountGrpcService = discountGrpcService;
+            //_publishEndpoint = publishEndpoint;
+            //_mapper = mapper;
         }
 
         [HttpGet("{userName}", Name = "GetBasket")]
@@ -37,11 +37,11 @@ namespace Basket.API.Controllers
             // TODO : Communicate with Discount.Grpc
             // and Calculate latest prices of product into shopping cart
             // consume Discount Grpc
-            foreach (var item in basket.Items)
-            {
-                var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
-                item.Price -= coupon.Amount;
-            }
+            //foreach (var item in basket.Items)
+            //{
+            //    var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
+            //    item.Price -= coupon.Amount;
+            //}
 
             return Ok(await _repository.UpdateBasket(basket));
         }
@@ -73,9 +73,9 @@ namespace Basket.API.Controllers
             }
 
             // send checkout event to rabbitmq
-            var eventMessage = _mapper.Map<BasketCheckoutEvent>(basketCheckout);
-            eventMessage.TotalPrice = basket.TotalPrice;
-            await _publishEndpoint.Publish(eventMessage);
+            //var eventMessage = _mapper.Map<BasketCheckoutEvent>(basketCheckout);
+            //eventMessage.TotalPrice = basket.TotalPrice;
+            //await _publishEndpoint.Publish(eventMessage);
 
             // remove the basket
             await _repository.DeleteBasket(basket.UserName);
