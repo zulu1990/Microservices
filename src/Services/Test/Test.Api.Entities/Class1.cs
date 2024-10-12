2ef023f569ce5ed02f71afad39f851d6ccb1228f
Archili Zivzivadze <Archili.Zivzivadze@nike.com>
2024-10-12 17:01:41 +0400

    multyproject docker compose

---
+++ src/Services/Test/Test.Api.Entities/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Test.Api.Entities
+{
+    public class Class1
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ src/Services/Test/Test.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using Test.Api.Entities;
+
+namespace Test.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "TEEEEEEEEEEst", "asdasdasdasd"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var test = new Class1();
+            return Enumerable.Range(1, 2).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+
+        [HttpGet("{name}")]
+        public IActionResult GetClass1(string name)
+        {
+            var ads = new Class1() { Name = "name is: " + name };
+
+            return Ok(ads);
+        }
+
+    }
+}
