c81a431c2558e6f0ffc35aa92f5c5955a7e1e19d
Archili Zivzivadze <Archili.Zivzivadze@nike.com>
2024-10-12 17:23:31 +0400

    14. Migrate PostreSQL Database When Discount Microservices Startup

---
+++ src/Services/Discount/Discount.Api/Extensions/ServiceExtensions.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Hosting;
+using Npgsql;
+
+namespace Discount.Api.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void MigrateDatabase<TContext>(this IServiceProvider services, int? retry = 0)
+        {
+            int retryForAvailability = retry.Value;
+
+            using var scope = services.CreateScope();
+            var configuration = services.GetRequiredService<IConfiguration>();
+            var logger = services.GetRequiredService<ILogger<TContext>>();
+
+            try
+            {
+                logger.LogInformation("Migrating postresql database.");
+
+                using var connection = new NpgsqlConnection
+                        (configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+                connection.Open();
+
+
+                using var command = new NpgsqlCommand
+                {
+                    Connection = connection
+                };
+
+                command.CommandText = "DROP TABLE IF EXISTS Coupon";
+                command.ExecuteNonQuery();
+
+                command.CommandText = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY, 
+                                                                ProductName VARCHAR(24) NOT NULL,
+                                                                Description TEXT,
+                                                                Amount INT)";
+                command.ExecuteNonQuery();
+
+                command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('IPhone X', 'IPhone Discount', 150);";
+                command.ExecuteNonQuery();
+
+                command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Samsung 10', 'Samsung Discount', 100);";
+                command.ExecuteNonQuery();
+
+                logger.LogInformation("Migrated postresql database.");
+
+
+
+            }
+            catch (NpgsqlException ex)
+            {
+                logger.LogError(ex, "An error occurred while migrating the postresql database");
+
+                if (retryForAvailability < 50)
+                {
+                    retryForAvailability++;
+                    Thread.Sleep(2000);
+                    MigrateDatabase<TContext>(services, retryForAvailability);
+                }
+            }
+        }
+    }
+}
--- src/Services/Discount/Discount.Api/Program.cs
+++ src/Services/Discount/Discount.Api/Program.cs
@@ -1,14 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
+using Discount.Api.Extensions;
+using Discount.Api.Repositories;
 
+var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
 
 var app = builder.Build();
+app.Services.MigrateDatabase<Program>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
